d1f3168d5d9dc5327c805ab7271ce1eb3faa69dc
Hemant Kumar Trigunayat <hemant@neevtech.com>
2014-08-20 19:54:49 +0530

    routes for the profile

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Catarse::Application.routes.draw do
       end
 
       resources :posts
-      get '/', to: 'profiles#show', as: :profile
+      # get '/', to: 'profiles#show', as: :profile
       get '/how-it-works', to: 'profiles#how_it_works', as: :about
       resource :profile
       # NOTE We use index instead of create to subscribe comming back from auth via GET
