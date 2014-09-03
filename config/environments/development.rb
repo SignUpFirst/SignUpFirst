5507c5312b028de44bef12c490d814fea38352ba
Hemant Kumar Trigunayat <hemant@neevtech.com>
2014-09-03 19:51:07 +0530

    from name resolved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,17 @@ Catarse::Application.configure do
   # mailcatcher configs
   config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.asset_host = "http://localhost:3000"
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            'roronlineshopping',
+    password:             'freshersror',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
 
